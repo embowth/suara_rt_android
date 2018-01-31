4d506a529171fbf9e79e5d0c376deb10ccde8991
embowth <embowth@gmail.com>
2018-01-31 16:54:05 +0700

    layanan

--- app/src/main/java/rw/rt/com/mykomplek/LayananActivity.java
+++ app/src/main/java/rw/rt/com/mykomplek/LayananActivity.java
@@ -1,13 +1,54 @@
 package rw.rt.com.mykomplek;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class LayananActivity extends AppCompatActivity {
+public class LayananActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    DatabaseHelper mDatabaseHelper;
+    String userId;
+
+    Spinner tipeLayanan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_layanan);
+
+        tipeLayanan = (Spinner)findViewById(R.id.spinnerKeperluan);
+        String[] items = new String[]{"Surat Pengantar"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        tipeLayanan.setAdapter(adapter);
+        tipeLayanan.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
